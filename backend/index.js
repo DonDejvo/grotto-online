8e917e9d925e86bb1465074169082af33057f1d1
DonDejvo <dada542cz@gmail.com>
2025-03-13 03:05:40 +0100

    update port

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,4 @@ import { SignalServer } from "./lancelot-signal-server/src/SignalServer.js";
 
 const signalServer = new SignalServer();
 
-signalServer.listen(8080);
+signalServer.listen(process.env.PORT || 8080);
